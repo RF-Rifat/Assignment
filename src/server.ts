97b28b642b5b9db791819736710e94789f99cb90
RF-Rifat <rfrifat6344@gmail.com>
2024-05-23 03:29:27 +0600

    project build command updated

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 import config from "./config";
 import app from "./app"; 
-import "dotenv";
 
 async function main() {
   try {
     await mongoose.connect(config.db_url as string);
-     const port = process.env.PORT || config.port;
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+    app.listen(config.port || 8080, () => {
+      console.log(`Server is running on port ${config.port|| 8080}`);
     });
   } catch (error) {
     console.error("Error starting server:", error);
